edfa7fc73a227676cf3dbdc190942328209f5035
Arturo Martínez Díaz <arturo.martinez@amartinez.dev>
2025-02-13 12:23:50 +0100

    feature (/src/pages): first navigation elements

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from 'next/link'
 
 export default function Document() {
     return (
         <Html>
-            <Head />
+            <Head>
+                <title>Zara Web Challenge</title>
+            </Head>
             <body>
-            <Main />
+            <aside>
+                <ul>
+                    <li>
+                        <Link href={'/'}>Characters</Link>
+                    </li>
+                    <li>
+                        <Link href={'/favourites'}>Favourites</Link>
+                    </li>
+                </ul>
+            </aside>
+            <Main/>
             <NextScript />
             </body>
         </Html>
