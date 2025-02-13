a8398976a720cb4221f4ab090facbff5ef7b2bef
Arturo Martínez Díaz <arturo.martinez@amartinez.dev>
2025-02-13 13:41:26 +0100

    feature (/src/*): replaced 'pages' with 'app' structure (2)

---
+++ src/app/favourites/index.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata =  {
+    title: 'My Favourite MARVEL Characters',
+}
+
+export default function Page() {
+    return <h1>Favourites</h1>
+}
--- src/pages/favourites/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <h1>Favourites</h1>
-}
