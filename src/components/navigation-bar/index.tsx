387a6fc3aa206223bd15632fd7a0e3b89d969f0f
Arturo Martínez Díaz <arturo.martinez@amartinez.dev>
2025-02-13 13:40:51 +0100

    feature (/src/*): added navigation-bar component

---
+++ src/components/navigation-bar/index.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export interface NavigationBarInput {
+    favourites: number;
+    loading: boolean;
+}
+
+export function NavigationBar({ favourites }: NavigationBarInput) {
+    return (<nav className={"navigation-bar"}>
+        <ul className={"navigation-bar__menu"}>
+            <li>
+                <Link href={"/"}>
+                    <Image src={"/assets/logo.png"} alt={"Marvel Characters"} width={130} height={52}/>
+                </Link>
+            </li>
+            <li className={"navigation-bar__empty"}>&nbsp;</li>
+            <li className={"navigation-bar__favourites"}>
+                <Link href={"/src/app/favourites"}>
+                    {
+                        favourites
+                            ? (<Image src={"/assets/heart_filled.png"} alt={"Favourites"} width={24} height={22}/>)
+                            : (<Image src={"/assets/heart_empty.png"} alt={"Favourites"} width={24} height={22}/>)
+                    }
+                </Link>
+                <span className={"navigation-bar__favourites-text"}>{ favourites }</span>
+            </li>
+        </ul>
+        <span className={"navigation-bar__loader"}></span>
+    </nav>);
+}
