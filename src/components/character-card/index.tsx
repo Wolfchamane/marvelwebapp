7aab2906eedec746a64e8b5d104437d9e285399e
Arturo Martínez Díaz <arturo.martinez@amartinez.dev>
2025-02-14 18:17:00 +0100

    feature (/src/components): new 'character-card' component

---
+++ src/components/character-card/index.tsx
@@ -0,0 +1,24 @@
+import './styles.sass';
+import Image from 'next/image';
+import { FavouriteIcon } from "@/components/index";
+
+export interface CharacterCardProperties {
+	name: string;
+	image: string;
+	isFavourite: boolean;
+}
+
+export function CharacterCard({ name, image, isFavourite }: CharacterCardProperties) {
+	return (
+		<div className={'character-card'}>
+			<div className={'character-card__image'}>
+				<Image src={image} alt={name} height={189.97} width={172.5}/>
+				<span className={'character-card__ribbon'}></span>
+			</div>
+			<div className={'character-card__name'}>
+				<span className={'character-card__name-text'}>{name}</span>
+				<FavouriteIcon filled={isFavourite}/>
+			</div>
+		</div>
+	);
+}
