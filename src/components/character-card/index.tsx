d94ddae210af7b4d2761437ec15fbeb4079631b0
Arturo Martínez Díaz <arturo.martinez@amartinez.dev>
2025-02-15 11:25:38 +0100

    feature (/src/**/character-card/index.tsx): improved component to do navigation into character details

--- src/components/character-card/index.tsx
+++ src/components/character-card/index.tsx
@@ -1,16 +1,17 @@
 import './styles.sass';
 import Image from 'next/image';
-import { FavouriteIcon } from '@/components/index';
+import Link from 'next/link';
 
 export interface CharacterCardProperties {
+	id: number;
 	name: string;
 	image: string;
 	isFavourite: boolean;
 }
 
-export function CharacterCard({ name, image, isFavourite }: CharacterCardProperties) {
+export function CharacterCard({ id, name, image, isFavourite }: CharacterCardProperties) {
 	return (
-		<div className={'character-card'}>
+		<Link className={'character-card'} href={`/character/${id}`}>
 			<div className={'character-card__image'}>
 				<Image src={image} alt={name} height={189.97} width={172.5} />
 				<span className={'character-card__ribbon'}></span>
@@ -23,6 +24,6 @@ export function CharacterCard({ name, image, isFavourite }: CharacterCardPropert
 						.join(' ')}
 				/>
 			</div>
-		</div>
+		</Link>
 	);
 }
