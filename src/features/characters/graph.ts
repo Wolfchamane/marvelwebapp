574bca909e88700a3c3fb4cb815c37912c2c6383
Arturo Martínez Díaz <arturo.martinez@amartinez.dev>
2025-02-15 11:26:13 +0100

    feature (/feature/characters/*.*): removed 'graph' file and updated used of CharacterCards component

--- src/features/characters/graph.ts
+++
@@ -1,6 +0,0 @@
-import { OutputHttpAdapter } from './adapters/output.http-adapter';
-import { DefaultCharactersUseCases } from './application/use-cases';
-import type { CharactersUseCases } from './types';
-
-export const provideCharactersUseCases = (): CharactersUseCases =>
-	new DefaultCharactersUseCases(new OutputHttpAdapter());
--- src/features/characters/index.tsx
+++ src/features/characters/index.tsx
@@ -1,10 +1,13 @@
-import type { FormEventHandler } from 'react';
 import './styles.sass';
 import { CharacterCard, SearchInput } from '@/components/index';
-import { provideCharactersUseCases } from './graph';
+import { OutputHttpAdapter } from './adapters/output.http-adapter';
+import { DefaultCharactersUseCases } from './application/use-cases';
+import type { CharactersUseCases } from './types';
+
+const provideCharactersUseCases = (): CharactersUseCases => new DefaultCharactersUseCases(new OutputHttpAdapter());
 
 export async function CharactersPage() {
-	const useCases = provideCharactersUseCases();
+	const useCases: CharactersUseCases = provideCharactersUseCases();
 	await useCases.fetchCharacters();
 
 	return (
@@ -17,6 +20,7 @@ export async function CharactersPage() {
 				{useCases.characters.map(item => (
 					<CharacterCard
 						key={`character-${item.$id}`}
+						id={item.$id}
 						name={item.name}
 						image={item.image}
 						isFavourite={item.isFavourite}
