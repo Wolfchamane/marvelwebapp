072299cb8367c3618e4d8e6f2de07dca0cf27950
Arturo Martínez Díaz <arturo.martinez@amartinez.dev>
2025-02-16 12:34:37 +0100

    feature (/characters/**/http-client.ts): improve interfaces

--- src/features/characters/infra/http-client.ts
+++ src/features/characters/infra/http-client.ts
@@ -15,43 +15,61 @@ export interface CharactersListComicsInput {
 	limit?: number;
 }
 
+export interface HttpClientOutput<T> {
+	response: InfraOutput<T> | undefined;
+	error?: XHRError;
+}
+
 export interface CharactersHttpClient {
-	list(input: CharactersListInput): Promise<InfraOutput<Character> | XHRError | undefined>;
-	describe(input: CharactersDescribeInput): Promise<InfraOutput<Character> | XHRError | undefined>;
-	listComics(input: CharactersListComicsInput): Promise<InfraOutput<Comic> | XHRError | undefined>;
+	list(input: CharactersListInput): Promise<HttpClientOutput<Character>>;
+	describe(input: CharactersDescribeInput): Promise<HttpClientOutput<Character>>;
+	listComics(input: CharactersListComicsInput): Promise<HttpClientOutput<Comic>>;
 }
 
 export class DefaultCharactersHttpClient extends DefaultXHR implements CharactersHttpClient {
-	list(input: CharactersListInput): Promise<InfraOutput<Character> | XHRError | undefined> {
-		return this.fetch<InfraOutput<Character>>('/characters', {
-			method: 'GET',
-			params: input,
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-		});
+	private _buildOutput<T>(response: InfraOutput<T> | XHRError | undefined): HttpClientOutput<T> {
+		return {
+			response: response && !('errorCode' in response) ? (response as InfraOutput<T>) : undefined,
+			error: response && 'errorCode' in response ? response : undefined,
+		};
 	}
 
-	describe(input: CharactersDescribeInput): Promise<InfraOutput<Character> | XHRError | undefined> {
-		return this.fetch<InfraOutput<Character>>('/characters/{id}', {
-			method: 'GET',
-			params: input,
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-		});
+	async list(input: CharactersListInput): Promise<HttpClientOutput<Character>> {
+		return this._buildOutput<Character>(
+			await this.fetch<InfraOutput<Character>>('/characters', {
+				method: 'GET',
+				params: input,
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+			})
+		);
 	}
 
-	listComics(input: CharactersListComicsInput): Promise<InfraOutput<Comic> | XHRError | undefined> {
-		return this.fetch<InfraOutput<Comic>>('/characters/{id}/comics', {
-			method: 'GET',
-			params: input,
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json',
-			},
-		});
+	async describe(input: CharactersDescribeInput): Promise<HttpClientOutput<Character>> {
+		return this._buildOutput<Character>(
+			await this.fetch<InfraOutput<Character>>('/characters/{id}', {
+				method: 'GET',
+				params: input,
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+			})
+		);
+	}
+
+	async listComics(input: CharactersListComicsInput): Promise<HttpClientOutput<Comic>> {
+		return this._buildOutput<Comic>(
+			await this.fetch<InfraOutput<Comic>>('/characters/{id}/comics', {
+				method: 'GET',
+				params: input,
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+				},
+			})
+		);
 	}
 }
