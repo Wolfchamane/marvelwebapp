869afe4caead5be43662a1bfd64e3af3214dac40
Arturo Martínez Díaz <arturo.martinez@amartinez.dev>
2025-02-19 12:18:59 +0100

    feature (/src/main.tsx): listen and handle 'DOMContentLoaded' event

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,42 @@ import { Provider } from 'react-redux';
 import { App } from './app';
 import { store } from './app/store';
 
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</StrictMode>
-);
+function loadServiceWorker(): void {
+	if ('serviceWorker' in navigator && import.meta.env.PROD) {
+		navigator.serviceWorker
+			.register('/src/sw.js', { type: 'module' })
+			.then(() => console.log(`serviceWorker ${import.meta.env.VITE_APP_VERSION} loaded!`))
+			.catch(error => console.error(`Error registering service worker:\n${error}`));
+	}
+}
+
+function loadApplication(): void {
+	createRoot(document.getElementById('root')!).render(
+		<StrictMode>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</StrictMode>
+	);
+}
+
+function toggleOffInitVeil():void {
+	const timeoutDelay: number = 500;
+	const className: string = 'init_veil';
+	const veil: HTMLElement|null = document.querySelector(`.${className}`);
+	if (veil) {
+		setTimeout(() => {
+			veil.classList.add(`${className}--fade`);
+			setTimeout(() => {
+				veil.classList.add(`${className}--hide`);
+				veil.classList.remove(`${className}--fade`);
+			}, timeoutDelay);
+		}, timeoutDelay);
+	}
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+	loadServiceWorker();
+	loadApplication();
+	toggleOffInitVeil();
+});
