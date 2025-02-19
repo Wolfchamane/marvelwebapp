59d12a02fc5584a681264b8db34183c9d0eb4b8c
Arturo Martínez Díaz <arturo.martinez@amartinez.dev>
2025-02-19 12:10:43 +0100

    feature (vite-env.d.ts): declare application environment variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,10 +4,9 @@ interface ImportMetaEnv {
 	readonly VITE_API_HOSTNAME: string;
 	readonly VITE_API_KEY: string;
 	readonly VITE_BASE_PATH: string;
+	readonly VITE_APP_VERSION: string;
 }
 
 interface ImportMeta {
 	readonly env: ImportMetaEnv;
 }
-
-declare const __IS_PRO__: string;
